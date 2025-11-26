d21c1e5eee8581268705a8e2cddc4978c789b349
shaw8386 <it.marketing.loki@gmail.com>
2025-11-26 21:46:58 +0700

    Add database connection setup in db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+// db.js
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: false }
+    : false,
+});
